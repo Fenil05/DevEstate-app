666f105bf229a5127c8bcd09b03ac90f01b78877
Fenil05 <fenilgajjar2265@gmail.com>
2024-04-03 18:36:42 +0530

    complete saved post functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,14 @@
 import express from "express"
-import { deleteUser, getUsers, getuser, savePost, updateUser } from "../controllers/user.controller.js"
+import { deleteUser, getUsers, getuser,profilePosts,savePost, updateUser } from "../controllers/user.controller.js"
 import {verifyToken} from "../middleware/verifyToken.js"
 
 const router = express.Router()
 
 router.get("/",getUsers)
-router.get("/:id",verifyToken,getuser)
+// router.get("/:id",verifyToken,getuser)
 router.put("/:id",verifyToken,updateUser)
 router.delete("/:id",verifyToken,deleteUser)
 router.post("/save",verifyToken,savePost)
+router.get("/profilePosts",verifyToken,profilePosts)
 
 export default router
